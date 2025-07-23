440f37d1ab0aeb7210b2fd5b1dd9496fb3c37a05
Benjamin Brooks <bjbrooks00@proton.me>
2025-07-22 20:34:58 -0400

    added planet shader seperate from the original hello triangle unlit color shader

--- res/shaders/hello_triangle_fragment.glsl
+++ res/shaders/hello_triangle_fragment.glsl
@@ -1,28 +1,7 @@
 #version 330 core
 
-in VS_OUT {
-  vec3 position;
-  vec3 normal;
-} fs_in;
-
 out vec4 FragColor;
 
-uniform sampler2D u_diffuse_map;
-
 void main() {
-  // object color
-  vec3 colorX = texture(u_diffuse_map, fs_in.position.zy).rgb;
-  vec3 colorY = texture(u_diffuse_map, fs_in.position.xz).rgb;
-  vec3 colorZ = texture(u_diffuse_map, fs_in.position.xy).rgb;
-
-  float blend_sharpness = 4;
-
-  vec3 blend_weight = pow(abs(normalize(fs_in.normal)), vec3(blend_sharpness));
-  blend_weight /= dot(blend_weight, vec3(1));
-
-  // vec3 color = fs_in.normal * 0.5;
-  vec3 color = colorX * blend_weight.x + colorY * blend_weight.y +
-    colorZ * blend_weight.z;
-
-  FragColor = vec4(color, 1.0);
+  FragColor = vec4(0.2, 0.3, 0.4, 1.0);
 }
--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ DEFINE_LIST(vec2)
 DEFINE_LIST(vec3)
 DEFINE_LIST(GLuint)
 
-static GLuint hello_triangle_shader = 0;
+static GLuint planet_shader = 0;
 static struct camera camera = {0};
 static struct mesh planet_mesh = {0};
 static GLuint planet_texture = 0;
@@ -50,9 +50,9 @@ void engine_scene_load(void) {
   glEnable(GL_CULL_FACE);
   glClearColor(0, 0, 0, 1);
 
-  hello_triangle_shader =
-      engine_shader_create("res/shaders/hello_triangle_vertex.glsl",
-                           "res/shaders/hello_triangle_fragment.glsl");
+  planet_shader =
+      engine_shader_create("res/shaders/planet_vertex.glsl",
+                           "res/shaders/planet_fragment.glsl");
 
   planet_texture = engine_texture_alloc("res/textures/moon_1.jpeg");
   camera = camera_alloc();
@@ -64,7 +64,7 @@ void engine_scene_unload(void) {
   glDeleteVertexArrays(1, &planet_mesh.VAO);
   glDeleteBuffers(1, &planet_mesh.VBOs[0]);
   glDeleteBuffers(1, &planet_mesh.EBO);
-  glDeleteProgram(hello_triangle_shader);
+  glDeleteProgram(planet_shader);
 }
 
 void engine_scene_update(void) {
@@ -96,12 +96,12 @@ void engine_scene_update(void) {
 
 void engine_scene_draw(void) {
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glUseProgram(hello_triangle_shader);
+  glUseProgram(planet_shader);
   // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
   {
     GLint camera_matrix_location =
-        glGetUniformLocation(hello_triangle_shader, "u_camera_matrix");
+        glGetUniformLocation(planet_shader, "u_camera_matrix");
     glUniformMatrix4fv(camera_matrix_location, 1, GL_FALSE, camera.matrix);
   }
 
@@ -110,13 +110,13 @@ void engine_scene_draw(void) {
     mathf_transform_matrix(transform_matrix, &planet_transform);
 
     GLint model_matrix_location =
-        glGetUniformLocation(hello_triangle_shader, "u_transform_matrix");
+        glGetUniformLocation(planet_shader, "u_transform_matrix");
     glUniformMatrix4fv(model_matrix_location, 1, GL_FALSE, transform_matrix);
   }
 
   glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_2D, planet_texture);
-  glUniform1i(glGetUniformLocation(hello_triangle_shader, "u_diffuse_map"), 0);
+  glUniform1i(glGetUniformLocation(planet_shader, "u_diffuse_map"), 0);
 
   glBindVertexArray(planet_mesh.VAO);
   glDrawElements(GL_TRIANGLES, planet_mesh.indices_count, GL_UNSIGNED_INT, 0);
